da8e8069a616766146c6dad4b19951dbe6d1d431
niziopawel <niziopawel2@gmail.com>
2020-02-24 12:29:27 +0100

    fix images and dropdowns on mobile devices, delete unnecessary files

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -8,14 +8,6 @@ const Header = () => {
         <div className="header__logo-box">
           All<span>Movies</span>
         </div>
-        <ul className="header__nav">
-          <li className="header__nav-item">
-            <a href="/#">Movies</a>
-          </li>
-          <li className="header__nav-item">
-            <a href="/#">Favorite</a>
-          </li>
-        </ul>
       </header>
     </div>
   )
